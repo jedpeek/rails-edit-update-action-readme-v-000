a78ea37b924e2984d603de993c8036b904428a37
Jed Peek <jedpeek@yahoo.com>
2018-08-15 20:50:39 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create]
 
   get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+  
   put 'posts/:id', to: 'posts#update'
 end
